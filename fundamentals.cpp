fb990b8e12ac3cf95887b032ae7490bedfb0d751
juwayriyah9114 <juwayriyahkahkashan9114@gmail.com>
2026-07-13 22:32:36 +0530

    fundamentals learned on 13/7/26

--- fundamentals.cpp
+++ fundamentals.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int i,n;
-    bool isprime=true;
-    cout<<"enter the value of n:";
-    cin>>n;
-   for(i=2;i<=n-1;i++)
-   {
-    if(n%i==0)
-    {
-        isprime=false;
-        break;
-    }
-   }
-   if(isprime==true)
-    cout<<"the number is prime"<<endl;
-   else
-    cout<<"the number is not prime"<<endl;
+//     int i,n;
+//     bool isprime=true;
+//     cout<<"enter the value of n:";
+//     cin>>n;
+//    for(i=2;i<=n-1;i++)
+//    {
+//     if(n%i==0)
+//     {
+//         isprime=false;
+//         break;
+//     }
+//    }
+//    if(isprime==true)
+//     cout<<"the number is prime"<<endl;
+//    else
+//     cout<<"the number is not prime"<<endl;
 
-    return 0;
+//     return 0;
+int i;
+for(i=1;i<=5;i++)
+    {
+        cout<<i<<endl;
+    }
 };

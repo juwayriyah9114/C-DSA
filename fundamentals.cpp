efea405bb5a1a479aa8d561fd6bd8af95ff98bd6
juwayriyah9114 <juwayriyahkahkashan9114@gmail.com>
2026-07-27 15:43:15 +0530

    fundamentals learned on 13/7/26

--- fundamentals.cpp
+++ fundamentals.cpp
@@ -1,18 +1,13 @@
+
 #include <iostream>
 using namespace std;
-int main(){
-    int a,b,i,j,k;
-    char ch='A';
-    cout <<"enter the numbr of rows and colums"<<endl;
-    cin>>a>>b;
-    for(i=1;i<=a;i++)
-        {
-            for(j=1;j<=i;j++)
-            {
-                cout<<ch;
-                ch++;
-            }
-             cout<<endl;
 
-        }
-    }
+void hello()
+{
+    cout <<"hello world";
+}
+int main()
+{
+    hello();
+    return 0;
+}

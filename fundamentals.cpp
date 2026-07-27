2fbea72282e0eb8c1c4b338e76fa04e93b87e80f
juwayriyah9114 <juwayriyahkahkashan9114@gmail.com>
2026-07-27 21:46:33 +0530

    fundamentals learned on 13/7/26

--- fundamentals.cpp
+++ fundamentals.cpp
@@ -4,20 +4,19 @@ using namespace std;
 
 int pro(int a)
 {
-    if(a%2==0)
-    {
-        cout<<"even";
-    }
-    else{
-        cout<<"odd";
-    }
-    return 0;
+    int i,ans=1;
+   for (i=1;i<=a;i++)
+   {
+    ans=ans*i;
+   }
+    return ans;
 }
 int main()
 {
-    int a;
+    int a,ans;
     cout<<"enter the value number:";
     cin>>a;
-    pro(a);
-
+    ans=pro(a);
+    cout<<ans;
+    return 0;
 }

1bf75d8f8f71f19ab6d37c41c04ae7afd899c1cb
juwayriyah9114 <juwayriyahkahkashan9114@gmail.com>
2026-07-29 20:48:20 +0530

    fundamentals learned on 29/7/26

--- fundamentals.cpp
+++ fundamentals.cpp
@@ -4,11 +4,24 @@ using namespace std;
 
 int main()
 {
-    int a=10;
-    cout<<a<< endl;
-    int *b=&a;
-    cout<<b<< endl;
-    cout<<*b<< endl;
-    return 0;
+   int n;
+   cout<<"enter the no. of elements to be stored in the array\n ";
+   cin>> n;
+   int arr[n];
+   for(int i=0;i<n;i++)
+    {
+        cout<<"enter the"<<i+1<<"value\n";
+        cin>>arr[i];
+    }
+    int max=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        if(max<arr[i])
+        {
+            max=arr[i];
+        }
+    }
+    cout<<"maximum value="<<max;
+
 
 }

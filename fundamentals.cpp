8251db957dbfddb17a1d4a5cadf1df98006e2037
juwayriyah9114 <juwayriyahkahkashan9114@gmail.com>
2026-07-29 14:53:31 +0530

    fundamentals learned on 29/7/26

--- fundamentals.cpp
+++ fundamentals.cpp
@@ -2,21 +2,13 @@
 #include <iostream>
 using namespace std;
 
-int pro(int a)
-{
-    int i,ans=1;
-   for (i=1;i<=a;i++)
-   {
-    ans=ans*i;
-   }
-    return ans;
-}
 int main()
 {
-    int a,ans;
-    cout<<"enter the value number:";
-    cin>>a;
-    ans=pro(a);
-    cout<<ans;
+    int a=10;
+    cout<<a<< endl;
+    int *b=&a;
+    cout<<b<< endl;
+    cout<<*b<< endl;
     return 0;
+
 }

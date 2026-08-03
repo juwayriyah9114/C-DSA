da8b66b61632b424bd7a6ea45357ac05c7393ae2
juwayriyah9114 <juwayriyahkahkashan9114@gmail.com>
2026-08-03 21:57:58 +0530

    fundamentals learned on 29/7/26

--- fundamentals.cpp
+++ fundamentals.cpp
@@ -1,27 +1,31 @@
 
 #include <iostream>
+#include <climits>
 using namespace std;
-
+void subarray(int *arr, int n)
+{
+   int maxSum = INT_MIN;
+   for(int start=0; start<n; start++)
+   {
+      for(int end=start; end<n; end++)
+      {
+         int cursum=0;
+         for(int i=start; i<=end; i++)
+         {
+            cursum=cursum+arr[i];
+         }
+         if(cursum>maxSum)
+         {
+            maxSum=cursum;
+         }
+      }
+      cout<<endl;
+   }
+   cout<<"Maximum sum of subarray is:" << maxSum << endl;
+}
 int main()
 {
-   int n;
-   cout<<"enter the no. of elements to be stored in the array\n ";
-   cin>> n;
-   int arr[n];
-   for(int i=0;i<n;i++)
-    {
-        cout<<"enter the"<<i+1<<"value\n";
-        cin>>arr[i];
-    }
-    int max=arr[0];
-    for(int i=1;i<n;i++)
-    {
-        if(max<arr[i])
-        {
-            max=arr[i];
-        }
-    }
-    cout<<"maximum value="<<max;
-
-
+   int arr[5]={1,2,3,4,5};
+   int n=5;
+   subarray(arr, n);
 }

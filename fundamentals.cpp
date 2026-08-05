0580145120578df9f209107f2e1c073140da430d
juwayriyah9114 <juwayriyahkahkashan9114@gmail.com>
2026-08-05 15:44:16 +0530

    fundamentals learned on 5/8/26

--- fundamentals.cpp
+++ fundamentals.cpp
@@ -1,31 +1,37 @@
 
 #include <iostream>
 #include <climits>
+   
 using namespace std;
-void subarray(int *arr, int n)
+int kadan(int *arr,int n)
 {
-   int maxSum = INT_MIN;
-   for(int start=0; start<n; start++)
+   int current_sum = 0;
+   int max_sum=INT_MIN;
+   for (int i=0;i<n;i++)
    {
-      for(int end=start; end<n; end++)
+      current_sum+=arr[i];
+      max_sum=max(max_sum,current_sum);
+      if(current_sum<0)
       {
-         int cursum=0;
-         for(int i=start; i<=end; i++)
-         {
-            cursum=cursum+arr[i];
-         }
-         if(cursum>maxSum)
-         {
-            maxSum=cursum;
-         }
+         current_sum=0;
       }
-      cout<<endl;
    }
-   cout<<"Maximum sum of subarray is:" << maxSum << endl;
+
+   return max_sum;
+
 }
 int main()
 {
-   int arr[5]={1,2,3,4,5};
-   int n=5;
-   subarray(arr, n);
+   int n,max_sum;
+   int arr[100]; // Assuming a maximum size of 100
+   cout<<"Enter the size of the array: ";
+   cin>>n;
+   cout<<"Enter the elements of the array: ";
+   for(int i=0; i<n; i++)
+   {
+      cin>>arr[i];
+   }
+   max_sum=kadan(arr,n);
+   cout<<"Maximum contiguous sum is: "<<max_sum<<endl;
+
 }

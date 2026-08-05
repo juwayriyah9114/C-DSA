d4c8afc0df8d779ed46226d07c95861a4a706f02
juwayriyah9114 <juwayriyahkahkashan9114@gmail.com>
2026-08-05 20:03:54 +0530

    fundamentals learned on 5/8/26

--- fundamentals.cpp
+++ fundamentals.cpp
@@ -3,35 +3,29 @@
 #include <climits>
    
 using namespace std;
-int kadan(int *arr,int n)
+int maxprofit(int *prices,int n)
 {
-   int current_sum = 0;
-   int max_sum=INT_MIN;
-   for (int i=0;i<n;i++)
+   int bestbuy[100000];
+   bestbuy[0]=INT_MAX;
+   for(int i=1;i<n;i++)
    {
-      current_sum+=arr[i];
-      max_sum=max(max_sum,current_sum);
-      if(current_sum<0)
-      {
-         current_sum=0;
-      }
+      bestbuy[i]=min(bestbuy[i-1],prices[i-1]);
+      
    }
-
-   return max_sum;
-
+   int maxprofit=0;
+   for(int i=0;i<n;i++)
+   {
+      int currentprofit=prices[i]-bestbuy[i];
+      maxprofit=max(maxprofit,currentprofit);
+   }
+   cout<<maxprofit<<endl;
 }
+
 int main()
 {
-   int n,max_sum;
-   int arr[100]; // Assuming a maximum size of 100
-   cout<<"Enter the size of the array: ";
-   cin>>n;
-   cout<<"Enter the elements of the array: ";
-   for(int i=0; i<n; i++)
-   {
-      cin>>arr[i];
-   }
-   max_sum=kadan(arr,n);
-   cout<<"Maximum contiguous sum is: "<<max_sum<<endl;
+   int prices[6]={7,1,5,3,6,4};
+   int n=sizeof(prices)/sizeof(int);
 
+   maxprofit(prices,n);
+   return 0;
 }

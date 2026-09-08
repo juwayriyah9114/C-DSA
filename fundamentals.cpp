6fb6907d03a29e8bb4ac232cc1f80a394e606032
juwayriyah9114 <juwayriyahkahkashan9114@gmail.com>
2026-09-08 23:34:02 +0530

    document_update

--- fundamentals.cpp
+++ fundamentals.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-void convert(char word[],int n)
-{
-   for(int i=0;i<n;i++)
-   {
-      char ch=word[i];
-      if(ch>='A' && ch<='Z')
-      {
-         continue;
-      }
-      else
-      {
-         word[i]=ch-'a'+'A';
-      }
-   }
-   
-}
 int main()
 {
-   char word[]="ApPle";
-   convert(word, strlen(word));
-   cout<<word;
-   return 0;
+   int size;
+   cout << "Enter the size of the string: ";
+   cin >> size;
+   int *arr=new int[size];
+   int x=1;
+   for(int i=0;i<size;i++)
+   {
+      arr[i]=x;
+     cout<<(arr[i])<<" ";
+      x++;
+
+   }
 
 }
